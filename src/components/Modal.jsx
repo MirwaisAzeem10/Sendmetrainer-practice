c9823c663555c723a845c9dec54e6090d14d747f
MirwaisAzeem10 <mirwais.azeem1@gmail.com>
2022-08-17 15:37:16 +0500

    modal page with scroll bar is done

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,121 +3,115 @@ import React from "react";
 function Modal() {
   return (
     <>
-      <button
-        class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-        data-modal-toggle="defaultModal"
-      >
-        Toggle modal
-      </button>
-
-      <div className="container mx-auto justify-start items-start">
+      <div className="container mx-auto">
         <p className="text-lg font-semibold">Review</p>
       </div>
 
-      <div className="flex items-center justify-center container mx-auto  flex-col-2 gap-4 mt-5">
-        <div className="flex  flex-col bg-[#BFBFBF4D] rounded-md w-[20%] h-[12rem] px-6">
-          <div className="flex justify-between gap-4 py-4">
-            <p className="text-[#BFBFBF]">M.Awais</p>
-            <p className="text-[#BFBFBF]">Apr 05</p>
+      <div className="overflow-y-auto   container mx-auto  border-[1px] border-solid rounded-md border-[#BFBFBF]  my-10">
+        <div className="overflow-auto flex items-center justify-center container mx-auto  flex-col-2 gap-4 my-5">
+          <div className="flex  flex-col bg-[#BFBFBF4D] rounded-md w-[20%] h-[12rem] px-6">
+            <div className="flex justify-between gap-4 py-4">
+              <p className="text-[#BFBFBF]">M.Awais</p>
+              <p className="text-[#BFBFBF]">Apr 05</p>
+            </div>
+            <div className="flex justify-start items-start gap-2 mt-2">
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+            </div>
+            <div className="mt-20">
+              <p className="text-sm">Texting</p>
+            </div>
           </div>
-          <div className="flex justify-start items-start gap-2 mt-2">
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
+
+          <div className="flex  flex-col bg-[#BFBFBF4D] rounded-md w-[20%] h-[12rem] px-6">
+            <div className="flex justify-between gap-4 py-4">
+              <p className="text-[#BFBFBF]">M.Awais</p>
+              <p className="text-[#BFBFBF]">Apr 05</p>
+            </div>
+            <div className="flex justify-start items-start gap-2 mt-2">
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+            </div>
+            <div className="mt-20">
+              <p className="text-sm">Texting </p>
+            </div>
           </div>
-          <div className="mt-20">
-            <p className="text-sm">Texting</p>
+        </div>
+        <div className="flex items-center justify-center container mx-auto  flex-col-2 gap-4 my-5">
+          <div className="flex  flex-col bg-[#BFBFBF4D] rounded-md w-[20%] h-[12rem] px-6">
+            <div className="flex justify-between gap-4 py-4">
+              <p className="text-[#BFBFBF]">M.Awais</p>
+              <p className="text-[#BFBFBF]">Apr 05</p>
+            </div>
+            <div className="flex justify-start items-start gap-2 mt-2">
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+            </div>
+            <div className="mt-20">
+              <p className="text-sm">Texting </p>
+            </div>
+          </div>
+
+          <div className="flex  flex-col bg-[#BFBFBF4D] rounded-md w-[20%] h-[12rem] px-6">
+            <div className="flex justify-between gap-4 py-4">
+              <p className="text-[#BFBFBF]">M.Awais</p>
+              <p className="text-[#BFBFBF]">Apr 05</p>
+            </div>
+            <div className="flex justify-start items-start gap-2 mt-2">
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+            </div>
+            <div className="mt-20">
+              <p className="text-sm">Texting </p>
+            </div>
           </div>
         </div>
 
-        <div className="flex  flex-col bg-[#BFBFBF4D] rounded-md w-[20%] h-[12rem] px-6">
-          <div className="flex justify-between gap-4 py-4">
-            <p className="text-[#BFBFBF]">M.Awais</p>
-            <p className="text-[#BFBFBF]">Apr 05</p>
+        <div className="flex items-center justify-center container mx-auto  flex-col-2 gap-4 my-5">
+          <div className="flex  flex-col bg-[#BFBFBF4D] rounded-md w-[20%] h-[12rem] px-6">
+            <div className="flex justify-between gap-4 py-4">
+              <p className="text-[#BFBFBF]">M.Awais</p>
+              <p className="text-[#BFBFBF]">Apr 05</p>
+            </div>
+            <div className="flex justify-start items-start gap-2 mt-2">
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+            </div>
+            <div className="mt-20">
+              <p className="text-sm">Texting </p>
+            </div>
           </div>
-          <div className="flex justify-start items-start gap-2 mt-2">
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-          </div>
-          <div className="mt-20">
-            <p className="text-sm">Texting </p>
-          </div>
-        </div>
-      </div>
-      <div className="flex items-center justify-center container mx-auto  flex-col-2 gap-4 mt-5">
-        <div className="flex  flex-col bg-[#BFBFBF4D] rounded-md w-[20%] h-[12rem] px-6">
-          <div className="flex justify-between gap-4 py-4">
-            <p className="text-[#BFBFBF]">M.Awais</p>
-            <p className="text-[#BFBFBF]">Apr 05</p>
-          </div>
-          <div className="flex justify-start items-start gap-2 mt-2">
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-          </div>
-          <div className="mt-20">
-            <p className="text-sm">Texting </p>
-          </div>
-        </div>
 
-        <div className="flex  flex-col bg-[#BFBFBF4D] rounded-md w-[20%] h-[12rem] px-6">
-          <div className="flex justify-between gap-4 py-4">
-            <p className="text-[#BFBFBF]">M.Awais</p>
-            <p className="text-[#BFBFBF]">Apr 05</p>
-          </div>
-          <div className="flex justify-start items-start gap-2 mt-2">
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-          </div>
-          <div className="mt-20">
-            <p className="text-sm">Texting </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex items-center justify-center container mx-auto  flex-col-2 gap-4 mt-5">
-        <div className="flex  flex-col bg-[#BFBFBF4D] rounded-md w-[20%] h-[12rem] px-6">
-          <div className="flex justify-between gap-4 py-4">
-            <p className="text-[#BFBFBF]">M.Awais</p>
-            <p className="text-[#BFBFBF]">Apr 05</p>
-          </div>
-          <div className="flex justify-start items-start gap-2 mt-2">
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-          </div>
-          <div className="mt-20">
-            <p className="text-sm">Texting </p>
-          </div>
-        </div>
-
-        <div className="flex  flex-col bg-[#BFBFBF4D] rounded-md w-[20%] h-[12rem] px-6">
-          <div className="flex justify-between gap-4 py-4">
-            <p className="text-[#BFBFBF]">M.Awais</p>
-            <p className="text-[#BFBFBF]">Apr 05</p>
-          </div>
-          <div className="flex justify-start items-start gap-2 mt-2">
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-            <img src="/images/Mask Group 316.svg" alt="" />
-          </div>
-          <div className="mt-20">
-            <p className="text-sm">Texting </p>
+          <div className="flex  flex-col bg-[#BFBFBF4D] rounded-md w-[20%] h-[12rem] px-6">
+            <div className="flex justify-between gap-4 py-4">
+              <p className="text-[#BFBFBF]">M.Awais</p>
+              <p className="text-[#BFBFBF]">Apr 05</p>
+            </div>
+            <div className="flex justify-start items-start gap-2 mt-2">
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+              <img src="/images/Mask Group 316.svg" alt="" />
+            </div>
+            <div className="mt-20">
+              <p className="text-sm">Texting </p>
+            </div>
           </div>
         </div>
       </div>
